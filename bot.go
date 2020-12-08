655b73fb0f60dc9d0792537374eba418debe3ba0
Joy <NULL>
2020-12-08 16:27:03 +0800

    新增注释

--- bot.go
+++ bot.go
@@ -4,16 +4,16 @@ import (
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
+//获取bot实例
 func GetBot(token string) (*tgbotapi.BotAPI, error) {
 	bot, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
 		return nil, err
 	}
-
 	return bot, nil
-
 }
 
+//发送消息
 func SendMsg(bot *tgbotapi.BotAPI, msg string, groupId int64) (tgbotapi.Message, error) {
 	return bot.Send(tgbotapi.NewMessage(groupId, msg))
 }
